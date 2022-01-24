7f2259a5be41264368eef74b55484c445e067d12
Hoang <hoangnh@pushsale.vn>
2022-01-24 14:32:19 +0700

    feat: init

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,31 +1,15 @@
-import * as React from 'react';
-
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-horizontal-calendar';
+import React, { useState } from 'react';
+import HorizontalCalendar from 'react-native-horizontal-calendar';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
+  const [selectedDate, setSelectedDate] = useState(new Date());
 
   return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-    </View>
+    <HorizontalCalendar
+      selectedDate={selectedDate}
+      setSelectedDate={(date) => {
+        setSelectedDate(date);
+      }}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
-  },
-});
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,132 @@
-export function multiply(a: number, b: number): Promise<number> {
-  return Promise.resolve(a * b);
+/* eslint-disable react-native/no-inline-styles */
+import React, { useMemo } from 'react';
+import {
+  View,
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
+
+const ITEM_WIDTH = 72;
+const ITEM_HEIGHT = 72;
+const ITEM_OFFSET = ITEM_WIDTH + 18;
+interface Props {
+  selectedDate: Date;
+  setSelectedDate: (date: Date) => void;
 }
+
+function dateAddDays(date: Date, days: number) {
+  var result = new Date(date);
+  result.setDate(result.getDate() + days);
+  return result;
+}
+
+function getDayString(date: Date): string {
+  return date.toString().split(' ')[0];
+}
+
+function isSameDay(date1: Date, date2: Date): boolean {
+  return date1.getDate() === date2.getDate();
+}
+
+function isToday(date: Date): boolean {
+  return new Date().getDate() === date.getDate();
+}
+
+function generateHorizontalCalendarDates(days: number): Date[] {
+  const today = new Date();
+  let result = [];
+  for (let i = 0; i < days; i++) {
+    result[i] = dateAddDays(today, i);
+  }
+
+  return result;
+}
+
+const HorizontalCalendar = ({ selectedDate, setSelectedDate }: Props) => {
+  const dates: Date[] = useMemo(() => {
+    return generateHorizontalCalendarDates(14);
+  }, []);
+
+  const onDatePress = (date: Date) => {
+    setSelectedDate(date);
+  };
+
+  const renderItem = ({ item }: { item: Date }) => {
+    const dayNumber = item.getDate();
+    const monthNumber = item.getMonth() + 1;
+    const dayString = getDayString(item);
+    const isActive = isSameDay(selectedDate, item);
+    return (
+      <TouchableOpacity
+        onPress={() => onDatePress(item)}
+        style={[
+          styles.item,
+          { borderWidth: 1, borderColor: isActive ? '#FE4014' : '#E8E8E8' },
+          isActive && { backgroundColor: 'white' },
+        ]}
+      >
+        <Text style={[styles.dayStyle, isActive && styles.activeText]}>
+          {isToday(item) ? 'today' : dayString}
+        </Text>
+        <View
+          style={{
+            width: 54,
+            height: 1,
+            backgroundColor: isActive ? '#FE4014' : '#E8E8E8',
+            marginVertical: 8,
+          }}
+        />
+        <Text style={[styles.dateOutput, isActive && styles.activeText]}>
+          {dayNumber}/{monthNumber}
+        </Text>
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <FlatList
+      data={dates}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.toDateString()}
+      horizontal={true}
+      contentContainerStyle={[
+        { paddingBottom: 16, paddingLeft: 4, paddingRight: 16 },
+      ]}
+      showsHorizontalScrollIndicator={false}
+      initialScrollIndex={dates.length - 8}
+      ItemSeparatorComponent={() => <View style={{ width: 18 }} />}
+      getItemLayout={(_, index) => ({
+        length: ITEM_WIDTH,
+        offset: ITEM_OFFSET * index,
+        index,
+      })}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  dateOutput: {
+    color: '#BDF0CC',
+    fontSize: 18,
+  },
+  dayStyle: {
+    color: '#BDF0CC',
+    textTransform: 'lowercase',
+  },
+  activeText: {
+    color: '#033F40',
+    fontWeight: 'bold',
+  },
+  item: {
+    width: ITEM_WIDTH,
+    height: ITEM_HEIGHT,
+    borderRadius: 12,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 8,
+  },
+});
+
+export default HorizontalCalendar;
